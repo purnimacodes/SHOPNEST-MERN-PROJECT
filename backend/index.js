31870fed5d71f8bc17c49f5dda3d06060b9bbc66
purnimacodes <purnimakri1080@gmail.com>
2026-09-05 13:12:45 +0530

    Add bcryptjs package and related type definitions
    - Created package.json and package-lock.json for backend with bcryptjs as a dependency.
    - Added type definitions for bcryptjs to improve TypeScript support.
    - Introduced a new package.json in the umd directory for commonjs compatibility.

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("ShopNest Backend is working properly!");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
